d5973baae81e343ee8b002849732b73168a13c46
belindawildhaber <belinda.wildhaber@stud.fhgr.ch>
2024-04-30 11:52:32 +0200

    Stand vor dem Mittag
    11:52
    extract needed information

--- extract.php
+++ extract.php
@@ -12,25 +12,10 @@ $output = curl_exec($ch);
 
 curl_close($ch);
 
-echo $output;
-
-echo "<br>";
-echo "<br>";
-
 // speichere hier alle Daten in Variablen
 $data = json_decode($output, true);
-// $latitude = $data[0]['latitude'];
-// $longitude = $data[0]['longitude'];
 
-// $temperature = $data[0]['current']['temperature_2m'];
-// $precipitation = $data[0]['current']['precipitation'];
-// $cloud_cover = $data[0]['current']['cloud_cover'];
-
-// echo "Temperature: $temperature, Precipitation: $precipitation, Cloud Cover: $cloud_cover" . "<br>";
-
-// echo "Latitude: $latitude, Longitude: $longitude";
-
-// make new array wirh all data
+// make new array wirh needed information
 $weather_data = [];
 foreach ($data as $item) {
     $latitude = $item['latitude'];
@@ -48,4 +33,12 @@ foreach ($data as $item) {
     ];
 }
 
-print_r($weather_data);
+// print_r($weather_data);
+// echo $weather_data[0]['latitude'];
+
+?>
+
+
+
+
+
---
+++ tranform.php
@@ -0,0 +1,10 @@
+<?php
+
+// include extract.php
+include 'extract.php';
+
+echo "Hello Transform!";
+
+print_r($weather_data);
+
+?>
