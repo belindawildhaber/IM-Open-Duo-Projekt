92c7232b964c06414ef781616203c4c6a536a208
belindawildhaber <belinda.wildhaber@stud.fhgr.ch>
2024-05-04 10:41:40 +0200

    MP Weather
    Finish Wetter Standorte...

--- scripts/currentWeatherView.js
+++ scripts/currentWeatherView.js
@@ -1,11 +1,42 @@
 fetchWeatherData().then( data => {
-    console.log(data);
-    let gradChur = document.getElementById('temperatureChur');
-    gradChur.textContent = data[0].temperature;
+    const cityChur = getObjectByCity(data, "Chur")
+    const cityBern = getObjectByCity(data, "Bern")
+    const cityZuerich = getObjectByCity(data, "Zürich")
+
+    document.getElementById('temperatureChur').textContent = cityChur.temperature + "C°";
+    document.getElementById('iconChur').src = getIconSrc(cityChur);
+    document.getElementById('temperatureBern').textContent = cityBern.temperature + "C°";
+    document.getElementById('iconBern').src = getIconSrc(cityBern);
+    document.getElementById('temperatureZuerich').textContent = cityZuerich.temperature + "C°";
+    document.getElementById('iconZuerich').src = getIconSrc(cityZuerich);
 }
     
 );
 
+function getIconSrc(obj){
+    switch(obj.weather_condition){
+        case "sonnig":
+            return "links/wmo_1.png";
+        case "leicht-bewoelkt":
+            return "links/wmo_2.png";
+        case "wolken&sonne":
+            return "links/wmo_3.png";
+        case "stark-bewoelkt":
+            return "links/wmo_4.png";
+        case "regen":
+            return "links/wmo_5.png";
+        case "starker-regen":
+            return "links/wmo_6.png";
+        case "schneefall":
+            return "links/wmo_7.png";
+        default:
+            return "";
+    }
+}
+
+function getObjectByCity(data, city){
+    return data.find(obj => obj.city === city);
+}
 
 async function fetchWeatherData(){
     try{
